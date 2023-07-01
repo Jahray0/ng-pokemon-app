7be7a356c506c94718e83f5997e5d6aa600897b8
Jahray0 <jaegle.maxime@gmail.com>
2023-07-01 18:00:13 +0200

    section 5 composants

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Liste de Pokémon</h1>`,
+  template: `<h1>Liste de Pokémons</h1>`,
   styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent implements OnInit {
 
-  pokemonsList : string[] = ['Bulbizarre', 'Salamèche', 'Carapuce'];
+  pokemonsList: Pokemon[] = POKEMONS;
 
   ngOnInit(): void {
     console.log(this.pokemonsList);
     console.table(this.pokemonsList);
-    this.selectPokemon('Bulbizarre');
+    this.selectPokemon(this.pokemonsList[0]);
 
   }
 
-  selectPokemon(pokemonName: string) {
-    console.log(`Vous avez cliqué sue le pokémon ${pokemonName}`);
+  selectPokemon(pokemon: Pokemon) {
+    console.log(`Vous avez cliqué sue le pokémon ${pokemon.name}`);
   }
 
 }
