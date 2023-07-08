b6b4e9d37ce03bca86224aa865540d10d3ad9603
Jahray0 <jaegle.maxime@gmail.com>
2023-07-08 15:40:24 +0200

    pokemon array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,22 +5,17 @@ import { Pokemon } from './pokemon';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-
 export class AppComponent implements OnInit {
-
   pokemonsList: Pokemon[] = POKEMONS;
 
   ngOnInit(): void {
-    console.log(this.pokemonsList);
     console.table(this.pokemonsList);
     this.selectPokemon(this.pokemonsList[0]);
-
   }
 
   selectPokemon(pokemon: Pokemon) {
     console.log(`Vous avez cliqué sue le pokémon ${pokemon.name}`);
   }
-
 }
