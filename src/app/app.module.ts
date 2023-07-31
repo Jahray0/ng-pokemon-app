524cef0bdc4d3c7e4d4bf31a094a185fe0cf8997
Jahray0 <jaegle.maxime@gmail.com>
2023-07-31 16:55:31 +0200

    maj directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SurvolDirective } from './survol.directive';
+import { BorderCardDirective } from './border-card.directive';
 
 //module racine de l'application
 //ou on importe les composants
@@ -11,7 +12,8 @@ import { SurvolDirective } from './survol.directive';
 @NgModule({
   declarations: [
     AppComponent,
-    SurvolDirective
+    SurvolDirective,
+    BorderCardDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/border-card.directive.ts
@@ -0,0 +1,34 @@
+import { Directive, HostListener, ElementRef, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appBorderCard]',
+})
+export class BorderCardDirective {
+  @Input() borderColor!: string;
+
+  initialColor: string = '#f5f5f5';
+  defaultColor: string = '#009688';
+  defaultHeight: number = 200;
+
+  constructor(private el: ElementRef) {
+    this.setHeight(this.defaultHeight);
+    this.setBorder(this.initialColor);
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.setBorder(this.borderColor || this.defaultColor);
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.setBorder(this.initialColor);
+  }
+
+  setHeight(height: number) {
+    this.el.nativeElement.style.height = height + 'px';
+  }
+
+  setBorder(borderColor: string) {
+    let border = 'solid 4px ' + borderColor;
+    this.el.nativeElement.style.border = border;
+  }
+}
